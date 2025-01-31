0f38e5487d2c58d3ee32d49bc6852db0dcbb45c2
سید حسن تقوی <taghavey.hassan@gmail.com>
2025-01-31 16:34:02 +0330

    main components added + add logo and background video to project + header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import { Icon } from 'react-icons-kit'
 import { home } from 'react-icons-kit/icomoon/home'
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+// import videoSrc from './vid/1.mp4';
 
 function App() {
   return (
-    <div>
-      <Icon size={32} icon={home} className='p-2 text-red-400'/>
+    <div className='flex flex-col w-full min-h-screen overflow-hidden bg-black'>
+      <video autoPlay muted loop className='absolute w-[100%] h-auto z-0'>    
+        <source src={require('./vid/2.mp4')} type="video/mp4"></source>  
+      </video>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
