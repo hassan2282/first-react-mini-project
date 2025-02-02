96242e5864b27f033dbfc85b66a540ac37692ffe
سید حسن تقوی <taghavey.hassan@gmail.com>
2025-02-02 21:14:55 +0330

    update css of forthpage

--- src/ForthPage.js
+++ src/ForthPage.js
@@ -13,12 +13,12 @@ function ForthPage() {
             </Fade>
               <div className='flex flex-row w-[38%] h-auto gap-2 justify-center' dir='rtl'>
                 <Fade right>
-                  <button className='basis-1/4 p-2 shadow-md shadow-gray-600 active:shadow-none text-white rounded-md bg-transparent backdrop-blur-lg border-2 border-white hover:bg-white hover:text-indigo-950 duration-150'>مشترک شدن</button>
-                  <input type='email' className='basis-3/4 rounded-md p-2 border-2 border-white backdrop-blur-xl bg-transparent text-white' placeholder='ایمیل خود را وارد کنید ...' />
+                  <button className='basis-1/4 p-3 shadow-md shadow-black active:shadow-none text-white rounded-md border-2 border-white hover:bg-white hover:text-indigo-950 duration-150'>ارسال</button>
+                  <input type='email' className='basis-3/4 rounded-md p-2 border-2 border-white  text-white' placeholder='ایمیل خود را وارد کنید ...' />
                 </Fade>
               </div>
               <Fade bottom cascade>
-                <ul className='flex flex-row w-[80%] *:text-white space-x-3 justify-center items-center hover:*:text-indigo-950 *:duration-300 *:cursor-pointer *:rounded-md *:border-2 *:border-white *:p-2 *:px-12'>
+                <ul className='flex flex-row w-[80%] *:text-white space-x-3 justify-center items-center hover:*:text-indigo-950 *:duration-300 *:cursor-pointer *:rounded-md *:border-2 *:border-white *:p-2 *:px-12 hover:*:bg-white'>
                     <li><Icon icon={facebook} size={40}/></li>
                     <li><Icon icon={twitter} size={40}/></li>
                     <li><Icon icon={youtube} size={40}/></li>
