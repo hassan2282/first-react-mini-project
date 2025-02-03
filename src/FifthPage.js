228e9ee8936e67fcb92cb35887c269d49b98ec20
سید حسن تقوی <taghavey.hassan@gmail.com>
2025-02-03 21:04:54 +0330

    add some info to fifth page

--- src/FifthPage.js
+++ src/FifthPage.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Icon from 'react-icons-kit'
+import {book} from 'react-icons-kit/fa/book'
 
 function FifthPage() {
   return (
-    <div>FifthPage</div>
+    <div className='flex flex-col w-screen h-[40rem] bg-zinc-900 items-center justify-center'>
+        <div className='flex flex-col w-[50%] h-auto items-center justify-center space-y-6'>
+        <Icon icon={book} size={40} className='text-white'/>
+        <h1 className='text-3xl text-gray-300'>سوالات متداول</h1>
+        <p className='text-center text-white'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز</p>
+        
+        </div>
+    </div>
   )
 }
 
