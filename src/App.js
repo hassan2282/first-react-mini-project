3b1c21ed979188f1a0c9dc925661b379278d7b5c
سید حسن تقوی <taghavey.hassan@gmail.com>
2025-02-06 20:53:53 +0330

    final and last commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import './App.css';
-import { Icon } from 'react-icons-kit'
-import { home } from 'react-icons-kit/icomoon/home'
 import Header from './Header';
 import Footer from './Footer';
 import Content from './Content';
+import { useEffect, useRef } from 'react';
 
 function App() {
+  const mouseCursorRef = useRef(null);
+
+  useEffect(() => {
+    const mouseCursor = mouseCursorRef.current;
+
+    const handleMouseMove = (e) => {
+      let x = e.pageX;
+      let y = e.pageY;
+      mouseCursor.style.top = (y - 30) + "px";
+      mouseCursor.style.left = (x - 35) + "px";
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
-    <div className='flex flex-col w-full min-h-screen overflow-hidden bg-black items-center scroll-smooth' style={{fontFamily: 'Vazir'}}>
+    <div className='flex flex-col w-full min-h-screen overflow-hidden bg-black items-center scroll-smooth' style={{ fontFamily: 'Vazir' }}>
+      <div ref={mouseCursorRef} id='mouseCursor' className='absolute rounded-full p-10 opacity-35 bg-purple-900 shadow-lg z-50 pointer-events-none'></div>
       <Header />
       <Content />
-      <Footer />
+      <Footer/>
     </div>
   );
 }
